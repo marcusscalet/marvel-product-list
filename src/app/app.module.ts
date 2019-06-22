e2bfa23d8331bf7602a9b2f255e52d6beff882d0
Marcus Scalet <marcus.scalet@hotmail.com>
2019-06-22 09:43:01 -0300

    adicionado primeng ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,111 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star-component';
+import { PrincipalComponent } from './principal/principal.component';
+
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    PrincipalComponent,
+    PanelMenuModule,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    PanelMenuModule,
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-pw pi-file',
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            { label: 'User', icon: 'pi pi-fw pi-user-plus' },
+            { label: 'Filter', icon: 'pi pi-fw pi-filter' }
+          ]
+        },
+        { label: 'Open', icon: 'pi pi-fw pi-external-link' },
+        { separator: true },
+        { label: 'Quit', icon: 'pi pi-fw pi-times' }
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'Help',
+        icon: 'pi pi-fw pi-question',
+        items: [
+          {
+            label: 'Contents',
+            icon: 'pi pi-pi pi-bars'
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-pi pi-search',
+            items: [
+              {
+                label: 'Text',
+                items: [
+                  {
+                    label: 'Workspace'
+                  }
+                ]
+              },
+              {
+                label: 'User',
+                icon: 'pi pi-fw pi-file',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'Actions',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+              { label: 'Save', icon: 'pi pi-fw pi-save' },
+              { label: 'Update', icon: 'pi pi-fw pi-save' },
+            ]
+          },
+          {
+            label: 'Other',
+            icon: 'pi pi-fw pi-tags',
+            items: [
+              { label: 'Delete', icon: 'pi pi-fw pi-minus' }
+            ]
+          }
+        ]
+      }
+    ];
+  }
+}
---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'pm-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
